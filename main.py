0e6e43ad67b4c5a6a9218eaeb2c9632a4aa30464
Alegz_13 <aleqsandre.kheladze@gmail.com>
2026-01-29 00:11:31 +0400

    Add function with try/except and loop

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def function():
-    x = int(input("შეიყვანეთ რიცხვი :"))
-    if x > 0:
-        print("არის კონტაქტი")
-    else:
-        print("alo? aloo")
-
+    while True:    
+        try:    
+            x = int(input("შეიყვანეთ რიცხვი :"))
+            if x > 0:
+                print("არის კონტაქტი")
+            else:
+                print("alo? aloo")
+            break    
+        except ValueError:
+            print("შეცდომა: გთხოვთ შეიყვანოთ მთელი რიცხვი სწორ ფორმატში")
 function()
